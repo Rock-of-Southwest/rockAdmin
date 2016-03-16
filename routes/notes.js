dbe9f2de728225ace470ed0018a02be44d3c71b1
annahuffman147 <annahuffman.plc@gmail.com>
2016-03-16 17:59:55 -0600

    crud routes for notes finished

--- routes/notes.js
+++ routes/notes.js
@@ -12,4 +12,41 @@ router.get('/', function(req, res, next) {
   });
 });
 
+//read one
+router.get('/:id', function(req, res, next){
+  Notes().select().where('id', req.params.id).then(function(notes){
+    res.status(200).send({notes: notes})
+  })
+})
+//create
+router.post('/', function(req, res, next){
+  Notes().insert({
+    id: req.body.id,
+    sermontitle: req.body.sermontitle,
+    item_type: req.body.item_type,
+    item_sequence: req.body.item_sequence,
+    item_text: req.body.item_text
+  }).then(function(result){
+    res.json(result)
+  })
+})
+//update
+router.put('/:id', function(req, res, next){
+  Notes().where('id', req.params.id).update({
+    sermontitle: req.body.sermontitle,
+    item_type: req.body.item_type,
+    item_sequence: req.body.item_sequence,
+    item_text: req.body.item_text
+  }).then(function(result){
+    res.json(result)
+  })
+})
+//delete
+router.delete('/:id', function(req, res, next){
+  Notes().where('id', req.params.id).del().then(function(result){
+    res.json(result)
+  })
+})
+
+
 module.exports = router;
