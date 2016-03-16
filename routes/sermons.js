1edd71c99f22ee491cfd9f53c7a7b620ecb5b402
annahuffman147 <annahuffman.plc@gmail.com>
2016-03-16 17:20:06 -0600

    crud finished for sermons

--- routes/sermons.js
+++ routes/sermons.js
@@ -5,11 +5,53 @@ var router = express.Router();
 function Sermon() {
   return knex('sermon');
 }
-
+//read all
 router.get('/', function(req, res, next) {
   Sermon().select().then(function(sermon){
-    res.status(200).send({sermon: sermon});
+    res.status(200).json({sermon: sermon});
   });
 });
+
+//read one
+router.get('/:sermon_title', function(req, res, next){
+  Sermon().select().where('sermon_title', req.params.sermon_title).then(function(sermon){
+    res.status(200).send({sermon: sermon})
+  })
+})
+//create
+router.post('/', function(req, res, next){
+  Sermon().insert({
+    seriestitle: req.body.seriestitle,
+    order: req.body.order,
+    sermon_title: req.body.sermon_title,
+    speaker: req.body.speaker,
+    video: req.body.video,
+    date_delivered: req.body.date_delivered,
+    active: req.body.active
+  }).then(function(result){
+    res.json(result)
+  })
+})
+//update
+router.put('/:sermon_title', function(req, res, next){
+  Sermon().where('sermon_title', req.params.sermon_title).update({
+    seriestitle: req.body.seriestitle,
+    order: req.body.order,
+    sermon_title: req.body.sermon_title,
+    speaker: req.body.speaker,
+    video: req.body.video,
+    date_delivered: req.body.date_delivered,
+    active: req.body.active
+  }).then(function(result){
+    res.json(result)
+  })
+})
+//delete
+router.delete('/:sermon_title', function(req, res, next){
+  Sermon().where('sermon_title', req.params.sermon_title).del().then(function(result){
+    res.json(result)
+  })
+})
+
 
 module.exports = router;
